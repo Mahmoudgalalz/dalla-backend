46601c86d11c4068ea238001d138c7864c71ec9a
Mahmoud Galal <ssss.aaa2002@gmail.com>
2025-01-08 01:37:14 +0200

    un-tight gurads from injection

--- src/professionals/professional.module.ts
+++ src/professionals/professional.module.ts
@@ -1,17 +1,9 @@
 import { PostgresPrismaService } from '@/config/prisma/postgres.services';
 import { PlatformAuthModule } from '@/shared/auth/platform/auth.module';
-import { ProfessionalAuthGuard } from '@/shared/auth/platform/guards/professionals-auth.guard';
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [PlatformAuthModule],
-  providers: [
-    PostgresPrismaService,
-    {
-      provide: APP_GUARD,
-      useClass: ProfessionalAuthGuard,
-    },
-  ],
+  providers: [PostgresPrismaService],
 })
 export class ProfessionalModule {}
