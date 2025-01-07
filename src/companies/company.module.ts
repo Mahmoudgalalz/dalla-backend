159e014f714d2fe472a7baa7ecf407d5dc0dd3fc
Mahmoud Galal <ssss.aaa2002@gmail.com>
2025-01-08 01:37:14 +0200

    un-tight gurads from injection

--- src/companies/company.module.ts
+++ src/companies/company.module.ts
@@ -1,21 +1,12 @@
 import { PostgresPrismaService } from '@/config/prisma/postgres.services';
 import { PlatformAuthModule } from '@/shared/auth/platform/auth.module';
-import { CompanyAuthGuard } from '@/shared/auth/platform/guards/company-auth.guard';
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
 import { CompanyController } from './company.controller';
 import { CompanyService } from './company.service';
 
 @Module({
   imports: [PlatformAuthModule],
   controllers: [CompanyController],
-  providers: [
-    CompanyService,
-    PostgresPrismaService,
-    {
-      provide: APP_GUARD,
-      useClass: CompanyAuthGuard,
-    },
-  ],
+  providers: [CompanyService, PostgresPrismaService],
 })
 export class CompanyModule {}
